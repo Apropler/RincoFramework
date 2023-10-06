397bebcd149c1233c6db00fa6e28e08d60230a64
Apropler <282664868@qq.com>
2023-10-06 11:46:40 +0800

    perf: 优化信号加载

--- addons/rinco_framework/BaseSignalBus.gd
+++ addons/rinco_framework/BaseSignalBus.gd
@@ -2,39 +2,38 @@ extends Node
 class_name BaseSignalBus
 
 var signal_classes = []	# 保存定义好的信号类BaseSignal
-var signal_nodes = {}
+var signals = {}
 
 # 实例化信号类
 func init_bus():
 	for s in signal_classes:
-		var node = s.new()
-		add_child(node)
-		var key = str(node.get_script())
-		signal_nodes[key] = node
+		var new_singal = s.new()
+		var key = str(new_singal.get_script())
+		signals[key] = new_singal
 		
 func connect_signal(signal_class, function: Callable):
 	var key = str(signal_class)
-	if key not in signal_nodes:
+	if key not in signals:
 		push_error("尝试连接未定义的信号.")
 		return
 	
-	var target = signal_nodes[key]
+	var target = signals[key]
 	target.connect_signal(function)
 
 func disconnect_signal(signal_class, function: Callable):
 	var key = str(signal_class)
-	if key not in signal_nodes:
+	if key not in signals:
 		push_error("尝试断开未定义的信号.")
 		return
 	
-	var target = signal_nodes[key]
+	var target = signals[key]
 	target.disconnect_signal(function)
 	
 func emit_signal_with_data(signal_class, data: Dictionary):
 	var key = str(signal_class)
-	if key not in signal_nodes:
+	if key not in signals:
 		push_error("尝试发送未定义的信号.")
 		return
 	
-	var target = signal_nodes[key]
+	var target = signals[key]
 	target.emit_signal_with_data(data)
